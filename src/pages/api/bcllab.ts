c97ae614a7e2a64c65eb28d9f345d79fc3c8e6b8
Brenno C. Lins <dev@bcl-st.com.br>
2022-11-14 19:25:29 -0300

    add API Routes: experimental-edge Basic Example

--- src/pages/api/bcllab.ts
+++ src/pages/api/bcllab.ts
@@ -1,9 +1,17 @@
 import type { NextRequest, NextResponse } from "next/server";
 
-export default function handler (request: NextRequest, response: NextResponse) {
-  return new Response(`Hello, from ${request.url} I'm now an Edge API Routes!`)
-}
+// export default function handler (request: NextRequest, response: NextResponse) {
+//   return new Response(`Hello, from ${request.url} I'm now an Edge API Routes!`)
+// }
+
+// export const config = {
+//   runtime: 'experimental-edge',
+// }
 
 export const config = {
   runtime: 'experimental-edge',
 }
+
+export default (req: NextRequest) => {
+  return new Response('Hello I am Dev Meditation!')
+} 
